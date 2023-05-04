403ed91c62e2e38d520bd36015dff9693f757759
joshuaautawi <autawijoshua@gmail.com>
2023-05-04 11:57:20 +0700

    setup on mysql

--- db/db.go
+++ db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InitDb() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:3306)/expense_tracker"
+	dsn := "root:password@tcp(127.0.0.1:3306)/expense_tracker"
 	DB, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		panic(err)
